3f5aaa035ec9d5e0fd359b24d6d938f96a9b1a1f
Murilo Zamaioli <murilo.zamaioli@gmail.com>
2025-06-26 20:57:42 -0300

    css finalizado

---
+++ app.js
@@ -0,0 +1,8 @@
+<script>
+  function rolarParaPosicao() {
+    window.scrollTo({
+      top: 2000, // posição em pixels na vertical
+      behavior: 'smooth' // rolagem suave
+    })
+  };
+</script>
